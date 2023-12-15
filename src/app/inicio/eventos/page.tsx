aa3f0f44f2e1053d00ffa26fa0f5fa06079e652a
Carlos-DE <carlosdominguezenriquez611@gmail.com>
2023-12-15 19:19:09 +0100

    eventos ojala funciones

--- src/app/inicio/eventos/page.tsx
+++ src/app/inicio/eventos/page.tsx
@@ -1,13 +1,17 @@
 "use client";
 
 import { useEffect, useState } from 'react';
-import Figure from "react-bootstrap/Figure";
+//import Figure from "react-bootstrap/Figure";
 
 interface Eventos {
   _id: string;
   imagen: string;
   nombre: string;
 }
+//no funciona porque el import da error
+//<Figure.Image
+//                    style={{ objectFit: "cover", objectPosition: "center", width: "100%", height: "250px" }}
+//                    src={`${registration.imagen}`} />
 
 function Directions() {
   const [Directions, setDirections] = useState<Eventos[]>([]);
@@ -34,9 +38,7 @@ function Directions() {
           <div key={registration._id} className="p-4 border rounded-md">
             <p>Nombre: {registration.nombre}</p>
             <p>Foto:</p><img src={registration.imagen}></img>
-            <Figure.Image
-                    style={{ objectFit: "cover", objectPosition: "center", width: "100%", height: "250px" }}
-                    src={`${registration.imagen}`} />
+            
           </div>
         ))}
       </div>
